ec466632d01eb63cb9c0b74f2825a266b4806886
sahilkakwani9 <sahkakwani@gmail.com>
2022-12-24 18:20:05 +0530

    fix: date difference

--- utils/getDifference.ts
+++ utils/getDifference.ts
@@ -3,10 +3,16 @@ function getDifference(timestamp: string) {
   const currentDate = date.getDate();
   const currentMonth = date.getMonth();
   const currentYear = date.getFullYear();
+  const currentSecond = date.getSeconds();
+  const currentMinute = date.getMinutes();
+  const currentHour = date.getHours();
   const notificationStamp = new Date(timestamp);
   const notificationDate = notificationStamp.getDate();
   const notificationMonth = notificationStamp.getMonth();
   const notificationYear = notificationStamp.getFullYear();
+  const notificationSecond = notificationStamp.getSeconds();
+  const notificationMinute = notificationStamp.getMinutes();
+  const notificationHour = notificationStamp.getHours();
 
   if (currentYear != notificationYear) {
     const difference = notificationYear - currentYear;
@@ -14,10 +20,20 @@ function getDifference(timestamp: string) {
   } else if (currentMonth != notificationMonth) {
     const difference = notificationMonth - currentMonth;
     return `~${difference} m ago`;
-  } else {
+  } else if (currentDate != notificationDate) {
     const difference = currentDate - notificationDate;
     return `~${difference}d ago`;
+  } else if (currentHour != notificationHour) {
+    const difference = currentHour - notificationHour;
+    return `~${difference}h ago`;
+  } else if (currentMinute != notificationMinute) {
+    const difference = currentMinute - notificationMinute;
+    return `~${difference} min ago`;
+  } else if (currentSecond != notificationSecond) {
+    const difference = currentSecond - notificationSecond;
+    return `~${difference} sec ago`;
   }
+
 }
 
 export default getDifference;
