da86a91fd406e4952cd9d01ba822c73e7b78a66f
Harsh <harshgajjar145@gmail.com>
2023-02-06 11:47:05 +0530

    fea: auth changes

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,6 +1,33 @@
-import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloLink, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from '@apollo/client/link/context';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+
+const getAccessToken = async () => {
+	const JsonData = await AsyncStorage.getItem("@storage_Key");
+	if (JsonData) {
+		const tokens = await JSON.parse(JsonData);
+		return `Bearer ${tokens.accessToken}`
+	}
+	return "";
+}
+
+const authLink = setContext((_, { headers }) => {
+	const token = getAccessToken();
+	return {
+		headers: {
+			...headers,
+			authorization: token,
+		}
+	}
+})
+
+const httpLink = createHttpLink({
+	uri: "https://api.lens.dev"
+})
+
 const client = new ApolloClient({
-	uri: "https://api.lens.dev",
+	link: authLink.concat(httpLink),
 	cache: new InMemoryCache(),
 });
 export { client };
