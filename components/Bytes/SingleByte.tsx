f617aa0a0393c5c5afff9a7a81a53618b2d7d383
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2023-02-21 10:15:41 +0530

    ui:shots ui fix

--- components/Bytes/SingleByte.tsx
+++ components/Bytes/SingleByte.tsx
@@ -7,6 +7,7 @@ import {
   Image,
   Share,
   Pressable,
+  ImageBackground,
 } from "react-native";
 import VideoPlayer from "expo-video-player";
 import {
@@ -56,152 +57,163 @@ const SingleByte = ({ item, index, currentIndex }: SingleByteProps) => {
     }
   };
   return (
-    <View
-      style={{
-        width: windowWidth,
-        height: windowHeight - bottomTabBarHeight,
-        position: "relative",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <StatusBar backgroundColor="transparent" />
-      <TouchableOpacity
-        activeOpacity={0.9}
-        onPress={() => setMute(!mute)}
-        style={{
-          width: "100%",
-          height: "100%",
-          position: "absolute",
-        }}
-      >
-        <VideoPlayer
-          videoProps={{
-            source: { uri: getIPFSLink(item.metadata.media[0]?.original?.url) },
-            shouldPlay: currentIndex === index ? true : false,
-            resizeMode: ResizeMode.CONTAIN,
-            isMuted: mute,
-            posterSource: {
-              uri: getIPFSLink(item?.metadata.cover),
-            },
-            posterStyle: {
-              resizeMode: ResizeMode.CONTAIN,
-            },
-          }}
-          autoHidePlayer={true}
-        />
-      </TouchableOpacity>
-      <Ionicons
-        name="volume-mute"
-        style={{
-          fontSize: mute ? 20 : 0,
-          color: "white",
-          position: "absolute",
-          backgroundColor: "rgba(52,52,52,0.6)",
-          borderRadius: 100,
-          padding: mute ? 20 : 0,
-        }}
-      />
+    <>
       <View
         style={{
-          position: "absolute",
           width: windowWidth,
-          zIndex: 1,
-          bottom: 0,
-          padding: 10,
-        }}
-      >
-        <View style={{ marginVertical: 10 }}>
-          <TouchableOpacity style={{ width: 180 }}>
-            <Text
-              style={{ color: "white", fontSize: 14, marginHorizontal: 10 }}
-            >
-              {item.metadata.name}
-            </Text>
-            <View
-              style={{ width: 100, flexDirection: "row", alignItems: "center" }}
-            >
-              <View
-                style={{
-                  width: 32,
-                  height: 32,
-                  borderRadius: 100,
-                  backgroundColor: "white",
-                  margin: 10,
-                }}
-              >
-                <Image
-                  source={{
-                    uri: getIPFSLink(item.profile?.picture?.original?.url),
-                  }}
-                  style={{
-                    width: "100%",
-                    height: "100%",
-                    resizeMode: "cover",
-                    borderRadius: 100,
-                  }}
-                />
-              </View>
-              <Text style={{ color: "white", fontSize: 16 }}>
-                {item.profile.name || item.profile.handle}
-              </Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View
-        style={{
-          position: "absolute",
-          bottom: bottomTabBarHeight,
-          right: 0,
+          height: windowHeight,
+          position: "relative",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-        <LikeButton
-          likes={likes}
-          id={item.id}
-          setLikes={setLikes}
-          isalreadyLiked={isalreadyLiked}
-          setisalreadyDisLiked={() => {}}
-          bytes={true}
+        <StatusBar backgroundColor="transparent" />
+        <TouchableOpacity
+          activeOpacity={0.9}
+          onPress={() => setMute(!mute)}
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+          }}
+        >
+          <VideoPlayer
+            defaultControlsVisible={false}
+            videoProps={{
+              source: {
+                uri: getIPFSLink(item.metadata.media[0]?.original?.url),
+              },
+              shouldPlay: currentIndex === index ? true : false,
+              resizeMode: ResizeMode.CONTAIN,
+              isMuted: mute,
+              posterSource: {
+                uri: getIPFSLink(item?.metadata.cover),
+              },
+              posterStyle: {
+                resizeMode: ResizeMode.CONTAIN,
+              },
+            }}
+            autoHidePlayer={true}
+          />
+        </TouchableOpacity>
+        <Ionicons
+          name="volume-mute"
+          style={{
+            fontSize: mute ? 20 : 0,
+            color: "white",
+            position: "absolute",
+            backgroundColor: "rgba(52,52,52,0.6)",
+            borderRadius: 100,
+            padding: mute ? 20 : 0,
+          }}
         />
-        <TouchableOpacity
+        <View
           style={{
+            position: "absolute",
+            width: windowWidth,
+            zIndex: 1,
+            bottom: 0,
             padding: 10,
+          }}
+        >
+          <View style={{ marginVertical: 10 }}>
+            <TouchableOpacity style={{ width: "auto", maxWidth: 250 }}>
+              <Text
+                style={{ color: "white", fontSize: 14, marginHorizontal: 10 }}
+                numberOfLines={2}
+              >
+                {item.metadata.name}
+              </Text>
+              <View
+                style={{
+                  width: "auto",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <View
+                  style={{
+                    width: 32,
+                    height: 32,
+                    borderRadius: 100,
+                    backgroundColor: "white",
+                    margin: 10,
+                  }}
+                >
+                  <Image
+                    source={{
+                      uri: getIPFSLink(item.profile?.picture?.original?.url),
+                    }}
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      resizeMode: "cover",
+                      borderRadius: 100,
+                    }}
+                  />
+                </View>
+                <Text style={{ color: "white", fontSize: 16 }}>
+                  {item.profile.name || item.profile.handle}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View
+          style={{
+            position: "absolute",
+            bottom: bottomTabBarHeight,
+            right: 0,
             justifyContent: "center",
             alignItems: "center",
           }}
         >
-          <EvilIcons name="comment" size={25} color="white" />
-          <Text style={{ color: "white" }}>
-            {item.stats.totalAmountOfComments}
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            padding: 10,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <CollectIcon height={23} width={23} />
-          <Text style={{ color: "white" }}>
-            {item.stats.totalAmountOfCollects}
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={{
-            padding: 10,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-          onPress={shareVideo}
-        >
-          <ShareIcon height={22} width={22} />
-        </TouchableOpacity>
+          <LikeButton
+            likes={likes}
+            id={item.id}
+            setLikes={setLikes}
+            isalreadyLiked={isalreadyLiked}
+            setisalreadyDisLiked={() => {}}
+            bytes={true}
+          />
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <EvilIcons name="comment" size={25} color="white" />
+            <Text style={{ color: "white" }}>
+              {item.stats.totalAmountOfComments}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <CollectIcon height={23} width={23} />
+            <Text style={{ color: "white" }}>
+              {item.stats.totalAmountOfCollects}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              justifyContent: "center",
+              alignItems: "center",
+              zIndex: 15,
+            }}
+            onPress={shareVideo}
+          >
+            <ShareIcon height={22} width={22} />
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </>
   );
 };
 
