30c0db71513f4fa9ef189b7bfc32b4a001a062ea
Harsh <harshgajjar145@gmail.com>
2023-03-09 19:47:42 +0530

    fixes

--- store/GuestStore.ts
+++ store/GuestStore.ts
@@ -2,7 +2,7 @@ import create from "zustand";
 import { IGuestStore } from "../types/Store";
 
 export const useGuestStore = create<IGuestStore>((set) => ({
-  isGuest: true,
+  isGuest: false,
   profileId: "0x5c59",
   handleGuest: (isGuest: boolean) =>
     set({
