58b170dcb736464bcc3d35a18bc89eb70339952f
Harsh <harshgajjar145@gmail.com>
2023-04-04 23:43:05 +0530

    fea: login with lens types

--- screens/Auth/LoginWithLens.tsx
+++ screens/Auth/LoginWithLens.tsx
@@ -9,12 +9,14 @@ import StyledText from "../../components/UI/StyledText";
 import { primary } from "../../constants/Colors";
 import { AUTH } from "../../constants/tracking";
 import { useAuthStore, useProfile, useToast } from "../../store/Store";
-import { RootStackScreenProps } from "../../types/navigation/types";
 import { ToastType } from "../../types/Store";
-import generateChallenge from "../../utils/lens/getChallenge";
-import getTokens from "../../utils/lens/getTokens";
-import storeTokens from "../../utils/storeTokens";
+import {
+  useAuthenticateMutation,
+  useChallengeLazyQuery,
+} from "../../types/generated";
+import { RootStackScreenProps } from "../../types/navigation/types";
 import TrackAction from "../../utils/Track";
+import storeTokens from "../../utils/storeTokens";
 
 function LoginWithLens({ navigation }: RootStackScreenProps<"LoginWithLens">) {
   const [isloading, setIsloading] = useState<boolean>(false);
@@ -23,25 +25,47 @@ function LoginWithLens({ navigation }: RootStackScreenProps<"LoginWithLens">) {
   const toast = useToast();
   const { setAccessToken, setRefreshToken } = useAuthStore();
 
+  const [
+    getChallenge,
+    { data: challange, error: challangeError, loading: challengeLoading },
+  ] = useChallengeLazyQuery();
+
+  const [
+    getTokens,
+    { data: tokens, error: tokensError, loading: tokenLoading },
+  ] = useAuthenticateMutation();
+
   const loginWithLens = async () => {
     setIsloading(true);
     try {
       const address = connector.accounts[0];
-      const challange = await generateChallenge({
-        address: address,
+      getChallenge({
+        variables: {
+          request: {
+            address: address,
+          },
+        },
       });
       const signature = await connector.sendCustomRequest({
         method: "personal_sign",
-        params: [address, challange?.text],
+        params: [address, challange?.challenge.text],
       });
       if (signature) {
-        const tokens = await getTokens({
-          address: address,
-          signature: signature,
+        getTokens({
+          variables: {
+            request: {
+              address: address,
+              signature: signature,
+            },
+          },
         });
-        setAccessToken(tokens?.accessToken);
-        setRefreshToken(tokens?.refreshToken);
-        await storeTokens(tokens?.accessToken, tokens?.refreshToken, false);
+        setAccessToken(tokens?.authenticate?.accessToken);
+        setRefreshToken(tokens?.authenticate?.refreshToken);
+        await storeTokens(
+          tokens?.authenticate?.accessToken,
+          tokens?.authenticate?.refreshToken,
+          false
+        );
         navigation.replace("Root");
         TrackAction(AUTH.SIWL);
       } else {
