8c67039c7772557c49f9cde8c0bac05348c3d7bd
sahilkakwani9 <sahkakwani@gmail.com>
2023-04-09 12:42:19 +0530

    fixes

--- components/UI/ImageCarousel.tsx
+++ components/UI/ImageCarousel.tsx
@@ -1,28 +1,28 @@
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import { Dimensions } from "react-native";
-import { interpolate, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from "react-native-reanimated";
-import { Animated } from "react-native";
+import Animated, { interpolate, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue } from "react-native-reanimated";
+// import { Animated } from "react-native";
 
 type Props = {
   data: any;
 };
 
 const ImageCarousel = ({ data }) => {
+  const [newData] = useState([
+    {key:'spacer-left'},
+    ...data,
+    {key:'spacer-right'}
+  ]);
   const windowWidth = Dimensions.get("window").width;
   const newSize = windowWidth * 0.7;
   const spacer=(windowWidth-newSize)/2;
   const x=useSharedValue(0);
   const onScroll=useAnimatedScrollHandler({
     onScroll:event=>{
-        x.value=event.contentOffset.x
-    }
-  })
-  const [newData] = useState([
-    {key:'spacer-left'},
-    ...data,
-    {key:'spacer-right'}
-  ])
+        x.value = event.contentOffset.x;
+    },
+  });
   return (
     <Animated.ScrollView 
         horizontal 
@@ -30,7 +30,7 @@ const ImageCarousel = ({ data }) => {
         bounces={false}
         scrollEventThrottle={16}
         snapToInterval={newSize}
-        decelerationRate={"fast"}
+        decelerationRate="fast"
         onScroll={onScroll}
     >
       {newData.map((item, index) => {
