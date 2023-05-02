52a4f397d33587bd8dc49b1cac39d290c2ff76f6
sahilkakwani9 <sahkakwani@gmail.com>
2023-05-02 16:37:51 +0530

    minor fix: get difference

--- utils/getDifference.ts
+++ utils/getDifference.ts
@@ -14,24 +14,36 @@ function getDifference(timestamp: Date | string) {
   if (interval > 1) {
     return interval + " years ago";
   }
+  else if (interval == 1){
+    return "~" + interval + " year ago";
+  }
 
   interval = Math.floor(seconds / 2592000);
   if (interval > 1) {
     return "~" + interval + " months ago";
+  }
+  else if (interval == 1){
+    return "~" + interval + " month ago";
   }
 
   interval = Math.floor(seconds / 86400);
   if (interval > 1) {
     return "~" + interval + " days ago";
   }
+  else if (interval == 1){
+    return "~" + interval + " day ago";
+  }
 
   interval = Math.floor(seconds / 3600);
   if (interval > 1) {
     return "~" + interval + " hours ago";
   }
+  else if (interval == 1){
+    return "~" + interval + " hour ago";
+  }
 
   interval = Math.floor(seconds / 60);
-  if (interval > 1) {
+  if (interval >= 1) {
     return "~" + interval + " min ago";
   }
 
