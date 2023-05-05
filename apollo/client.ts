56f362eddf6e16f25ff290dc0a8410b09f5d2fee
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2023-05-05 23:05:17 +0530

    fea:basic cache merge added

--- apollo/client.ts
+++ apollo/client.ts
@@ -1,6 +1,23 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
   uri: "https://api-mumbai.lens.dev",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Profile: {
+        fields: {
+          dispatcher: {
+            merge(existing = [], incoming: any) {
+              return { ...existing, ...incoming };
+            },
+          },
+          stats: {
+            merge(existing = [], incoming: any) {
+              return { ...existing, ...incoming };
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 export { client };
