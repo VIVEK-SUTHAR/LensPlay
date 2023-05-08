538796df46ee0cd8153e0c957bff9a5958da03a5
Umang Patel <umangpatel3105@gmail.com>
2023-05-08 20:41:16 +0530

    arrow bug fixed

--- components/Login/Onboarding.tsx
+++ components/Login/Onboarding.tsx
@@ -33,14 +33,15 @@ const Onboarding = ({
   const x = useSharedValue(0);
   const { width: SCREEN_WIDTH } = useWindowDimensions();
   const scaleValue = useSharedValue(0);
-
+  console.log("kya he:",SCREEN_WIDTH);
+  
   const arrowAnimationStyle = useAnimatedStyle(() => {
     return {
-      display: x.value > 720 ? "none" : "flex",
+      display: x.value >=SCREEN_WIDTH*3 ? "none" : "flex",
       transform: [
         {
           scale:
-            x.value > 720
+            x.value >= SCREEN_WIDTH*3
               ? withSpring((scaleValue.value = 0))
               : withSpring((scaleValue.value = 1)),
         },
@@ -50,11 +51,11 @@ const Onboarding = ({
 
   const ButtonAnimationStyle = useAnimatedStyle(() => {
     return {
-      display: x.value > 720 ? "flex" : "none",
+      display: x.value >= SCREEN_WIDTH*3? "flex" : "none",
       transform: [
         {
           scale:
-            x.value > 720
+            x.value >= SCREEN_WIDTH*3
               ? withSpring((scaleValue.value = 1), { mass: 0.7, stiffness: 90 })
               : withSpring((scaleValue.value = 0)),
         },
@@ -64,16 +65,16 @@ const Onboarding = ({
 
   const ButtonContainerStyle = useAnimatedStyle(() => {
     return {
-      width: x.value > 720 ? "100%" : "auto",
+      width: x.value >= SCREEN_WIDTH*3? "100%" : "auto",
       height: 54,
     };
   });
 
   const PaginatorStyle = useAnimatedStyle(() => {
     return {
-      display: x.value > 720 ? "none" : "flex",
+      display: x.value >= SCREEN_WIDTH*3 ? "none" : "flex",
       opacity:
-        x.value > 720
+        x.value >= SCREEN_WIDTH*3
           ? withTiming(0, {
               duration: 1000,
             })
@@ -91,19 +92,20 @@ const Onboarding = ({
     //THIS CODE CAN BREAK,PLEASE USE IT ON YOUR OWN RISK
     if (x?.value <= 0) {
       flatListRef?.current?.scrollToIndex({ index: 1 });
-    } else if (x?.value <= 360) {
+    } else if (x?.value <= SCREEN_WIDTH) {
       flatListRef?.current?.scrollToIndex({ index: 2 });
-    } else if (x?.value <= 720) {
+    } else if (x?.value <= SCREEN_WIDTH*2) {
       flatListRef?.current?.scrollToIndex({ index: 3 });
     }
-    // } else if (x.value > 720) {
-    //   flatListRef?.current?.scrollToIndex({ index: 4 });
-    // }
+    else if (x.value <= SCREEN_WIDTH*3) {
+      flatListRef?.current?.scrollToIndex({ index: 4 });
+    }
   };
 
   const onScroll = React.useCallback(
     (e: NativeSyntheticEvent<NativeScrollEvent>) => {
-      x.value = e.nativeEvent.contentOffset.x;      
+      x.value = e.nativeEvent.contentOffset.x; 
+      console.log(x.value);           
     },
     []
   );
