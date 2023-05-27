a4651138cf48bb166509bddba9f9b3a67d489e34
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2023-05-27 22:47:14 +0530

    fix:eslint  fixes

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,5 @@
-import {
-  AndroidManifest,
-  ConfigPlugin,
-  withAndroidManifest,
-} from "@expo/config-plugins";
-import { ExpoConfig } from "@expo/config-types";
+import { type AndroidManifest, type ConfigPlugin, withAndroidManifest } from "@expo/config-plugins";
+import { type ExpoConfig } from "@expo/config-types";
 import xml2js from "xml2js";
 
 const queriesXml = `
@@ -22,31 +18,29 @@ const queriesXml = `
   </intent>
 </queries>`;
 
-type KeyValuePair = {
-  $: {
-    [key: string]: string | undefined;
-  };
-};
+interface KeyValuePair {
+	$: Record<string, string | undefined>;
+}
 
-type Intent = {
-  action?: KeyValuePair[];
-  data?: KeyValuePair[];
-};
+interface Intent {
+	action?: KeyValuePair[];
+	data?: KeyValuePair[];
+}
 
-type Queries = {
-  intent?: Intent[];
-};
+interface Queries {
+	intent?: Intent[];
+}
 
-type ParseResult = {
-  queries: Queries;
-};
+interface ParseResult {
+	queries: Queries;
+}
 
 type AndroidManifestWithQuery = AndroidManifest & {
-  manifest: {
-    $: {
-      ["queries"]?: any;
-    };
-  };
+	manifest: {
+		$: {
+			["queries"]?: any;
+		};
+	};
 };
 
 /**
@@ -62,146 +56,148 @@ type AndroidManifestWithQuery = AndroidManifest & {
  * @see https://docs.expo.dev/workflow/configuration/
  * @see https://docs.expo.dev/guides/config-plugins/#modifying-the-androidmanifestxml
  */
-const addQueryToManifest = (androidManifest: AndroidManifestWithQuery) => {
-  const { manifest } = androidManifest;
-  let packageQuery: Queries;
+const addQueryToManifest = (
+	androidManifest: AndroidManifestWithQuery
+): AndroidManifestWithQuery => {
+	const { manifest } = androidManifest;
+	let packageQuery: Queries;
 
-  xml2js.parseString(queriesXml, (err, result: ParseResult) => {
-    packageQuery = result.queries;
+	xml2js.parseString(queriesXml, (_err, result: ParseResult) => {
+		packageQuery = result.queries;
 
-    if (!Array.isArray(manifest.$["queries"])) {
-      manifest.$["queries"] = [];
-    }
+		if (!Array.isArray(manifest.$.queries)) {
+			manifest.$.queries = [];
+		}
 
-    manifest.$["queries"].push(packageQuery);
-  });
+		manifest.$.queries.push(packageQuery);
+	});
 
-  return androidManifest;
+	return androidManifest;
 };
 
 const withPackageVisibility: ConfigPlugin = (config) => {
-  return withAndroidManifest(config, (config) => {
-    config.modResults = addQueryToManifest(config.modResults);
-    return config;
-  });
+	return withAndroidManifest(config, (config) => {
+		config.modResults = addQueryToManifest(config.modResults);
+		return config;
+	});
 };
 
 const config: ExpoConfig = {
-  name: "LensPlay",
-  slug: "test",
-  version: "0.0.3",
-  // orientation: "portrait",
-  backgroundColor: "#000000",
-  icon: "./assets/images/icon.png",
-  scheme: "lensplay",
-  userInterfaceStyle: "automatic",
-  splash: {
-    image: "./assets/images/adaptive-icon.png",
-    resizeMode: "contain",
-    backgroundColor: "#000000",
-  },
-  updates: {
-    fallbackToCacheTimeout: 0,
-    url: "https://u.expo.dev/2a7e6b8e-3f77-43ee-adeb-2d04c2c03a3f",
-  },
-  runtimeVersion: {
-    policy: "sdkVersion",
-  },
-  assetBundlePatterns: ["**/*"],
-  ios: {
-    supportsTablet: false,
-    requireFullScreen: true,
-    bundleIdentifier: "com.lensplayxyz.lensplay",
-    buildNumber: "1.0.0",
-    icon: "./assets/images/icon.png",
-    backgroundColor: "#000000",
-    googleServicesFile: "./GoogleService-Info.plist",
-  },
-  jsEngine: "hermes",
-  android: {
-    package: "com.lensplayxyz.lensplay",
-    versionCode: 5,
-    googleServicesFile: "./google-services.json",
-    adaptiveIcon: {
-      foregroundImage: "./assets/images/adaptive-icon.png",
-      backgroundColor: "#000000",
-    },
-    intentFilters: [
-      {
-        action: "VIEW",
-        data: [
-          {
-            scheme: "lensplay",
-          },
-        ],
-        category: ["BROWSABLE", "DEFAULT"],
-      },
-      {
-        action: "VIEW",
-        autoVerify: true,
-        data: [
-          {
-            scheme: "https",
-            host: "www.lensplay.xyz",
-            pathPrefix: "/watch/",
-          },
-        ],
-        category: ["BROWSABLE", "DEFAULT"],
-      },
-      {
-        action: "VIEW",
-        autoVerify: true,
-        data: [
-          {
-            scheme: "https",
-            host: "www.lensplay.xyz",
-            pathPrefix: "/channel/",
-          },
-        ],
-        category: ["BROWSABLE", "DEFAULT"],
-      },
-    ],
-  },
-  web: {
-    favicon: "./assets/images/favicon.png",
-  },
-  extra: {
-    // eas: {
-    //   projectId: "e7d8274d-41e8-4289-a3f3-00e5e8cac9fc",
-    // },
-  },
-  plugins: [
-    [
-      "expo-image-picker",
-      {
-        photosPermission:
-          "The app accesses your gallery to let you share them with your friends on Lens",
-      },
-    ],
-    [
-      "expo-camera",
-      {
-        cameraPermission: "Allow LensPlay to access your camera.",
-      },
-    ],
-    [
-      "expo-screen-orientation",
-      {
-        initialOrientation: "DEFAULT",
-      },
-    ],
-    [
-      "expo-build-properties",
-      {
-        ios: {
-          useFrameworks: "static",
-        },
-      },
-    ],
-    "@react-native-firebase/app",
-    "@react-native-firebase/perf",
-    "@react-native-firebase/crashlytics",
-  ],
+	name: "LensPlay",
+	slug: "test",
+	version: "0.0.3",
+	// orientation: "portrait",
+	backgroundColor: "#000000",
+	icon: "./assets/images/icon.png",
+	scheme: "lensplay",
+	userInterfaceStyle: "automatic",
+	splash: {
+		image: "./assets/images/adaptive-icon.png",
+		resizeMode: "contain",
+		backgroundColor: "#000000",
+	},
+	updates: {
+		fallbackToCacheTimeout: 0,
+		url: "https://u.expo.dev/2a7e6b8e-3f77-43ee-adeb-2d04c2c03a3f",
+	},
+	runtimeVersion: {
+		policy: "sdkVersion",
+	},
+	assetBundlePatterns: ["**/*"],
+	ios: {
+		supportsTablet: false,
+		requireFullScreen: true,
+		bundleIdentifier: "com.lensplayxyz.lensplay",
+		buildNumber: "1.0.0",
+		icon: "./assets/images/icon.png",
+		backgroundColor: "#000000",
+		googleServicesFile: "./GoogleService-Info.plist",
+	},
+	jsEngine: "hermes",
+	android: {
+		package: "com.lensplayxyz.lensplay",
+		versionCode: 5,
+		googleServicesFile: "./google-services.json",
+		adaptiveIcon: {
+			foregroundImage: "./assets/images/adaptive-icon.png",
+			backgroundColor: "#000000",
+		},
+		intentFilters: [
+			{
+				action: "VIEW",
+				data: [
+					{
+						scheme: "lensplay",
+					},
+				],
+				category: ["BROWSABLE", "DEFAULT"],
+			},
+			{
+				action: "VIEW",
+				autoVerify: true,
+				data: [
+					{
+						scheme: "https",
+						host: "www.lensplay.xyz",
+						pathPrefix: "/watch/",
+					},
+				],
+				category: ["BROWSABLE", "DEFAULT"],
+			},
+			{
+				action: "VIEW",
+				autoVerify: true,
+				data: [
+					{
+						scheme: "https",
+						host: "www.lensplay.xyz",
+						pathPrefix: "/channel/",
+					},
+				],
+				category: ["BROWSABLE", "DEFAULT"],
+			},
+		],
+	},
+	web: {
+		favicon: "./assets/images/favicon.png",
+	},
+	extra: {
+		// eas: {
+		//   projectId: "e7d8274d-41e8-4289-a3f3-00e5e8cac9fc",
+		// },
+	},
+	plugins: [
+		[
+			"expo-image-picker",
+			{
+				photosPermission:
+					"The app accesses your gallery to let you share them with your friends on Lens",
+			},
+		],
+		[
+			"expo-camera",
+			{
+				cameraPermission: "Allow LensPlay to access your camera.",
+			},
+		],
+		[
+			"expo-screen-orientation",
+			{
+				initialOrientation: "DEFAULT",
+			},
+		],
+		[
+			"expo-build-properties",
+			{
+				ios: {
+					useFrameworks: "static",
+				},
+			},
+		],
+		"@react-native-firebase/app",
+		"@react-native-firebase/perf",
+		"@react-native-firebase/crashlytics",
+	],
 };
 
 export default withPackageVisibility(config);
--- screens/BottomTabs/Explore/Trending.tsx
+++ screens/BottomTabs/Explore/Trending.tsx
@@ -1,259 +1,250 @@
 import * as React from "react";
 import { useEffect, useState } from "react";
 import {
-  ActivityIndicator,
-  FlatList,
-  Pressable,
-  RefreshControl,
-  SafeAreaView,
-  ScrollView,
+	ActivityIndicator,
+	FlatList,
+	Pressable,
+	RefreshControl,
+	SafeAreaView,
+	ScrollView,
 } from "react-native";
-import NotFound from "../../../components/Profile/NotFound";
+import ErrorMessage from "../../../components/common/ErrorMesasge";
+import Skeleton from "../../../components/common/Skeleton";
 import StyledText from "../../../components/UI/StyledText";
 import VideoCardSkeleton from "../../../components/UI/VideoCardSkeleton";
 import VideoCard from "../../../components/VideoCard";
-import ErrorMessage from "../../../components/common/ErrorMesasge";
 import { dark_primary } from "../../../constants/Colors";
+import { EXPLORE } from "../../../constants/tracking";
 import { useGuestStore } from "../../../store/GuestStore";
 import { useAuthStore, useProfile, useThemeStore } from "../../../store/Store";
 import {
-  Mirror,
-  Post,
-  PublicationMainFocus,
-  PublicationSortCriteria,
-  PublicationTypes,
-  useExploreQuery,
+	PublicationMainFocus,
+	PublicationSortCriteria,
+	PublicationTypes,
+	useExploreQuery,
+	type Mirror,
+	type Post,
 } from "../../../types/generated";
-import { RootTabScreenProps } from "../../../types/navigation/types";
-import Skeleton from "../../../components/common/Skeleton";
+import { type RootTabScreenProps } from "../../../types/navigation/types";
 import TrackAction from "../../../utils/Track";
-import { EXPLORE } from "../../../constants/tracking";
 
 type Explore = Post | Mirror;
 
-export default function Trending({
-  navigation,
-}: RootTabScreenProps<"Trending">) {
-  const tags = [
-    {
-      name: PublicationSortCriteria.Latest,
-      active: true,
-    },
-    {
-      name: PublicationSortCriteria.TopCommented,
-      active: false,
-    },
-    {
-      name: PublicationSortCriteria.TopCollected,
-      active: false,
-    },
-    {
-      name: PublicationSortCriteria.TopMirrored,
-      active: false,
-    },
-    {
-      name: PublicationSortCriteria.CuratedProfiles,
-      active: false,
-    },
-  ];
+export default function Trending({ navigation }: RootTabScreenProps<"Trending">): JSX.Element {
+	const tags = [
+		{
+			name: PublicationSortCriteria.Latest,
+			active: true,
+		},
+		{
+			name: PublicationSortCriteria.TopCommented,
+			active: false,
+		},
+		{
+			name: PublicationSortCriteria.TopCollected,
+			active: false,
+		},
+		{
+			name: PublicationSortCriteria.TopMirrored,
+			active: false,
+		},
+		{
+			name: PublicationSortCriteria.CuratedProfiles,
+			active: false,
+		},
+	];
 
-  const [currentTag, setCurrentTag] = useState<{
-    name: PublicationSortCriteria;
-    active: boolean;
-  }>(tags[0]);
-  const theme = useThemeStore();
-  const { accessToken } = useAuthStore();
-  const { currentProfile } = useProfile();
-  const { isGuest, profileId } = useGuestStore();
-  const [refreshing, setRefreshing] = useState<boolean>(false);
+	const [currentTag, setCurrentTag] = useState<{
+		name: PublicationSortCriteria;
+		active: boolean;
+	}>(tags[0]);
+	const theme = useThemeStore();
+	const { accessToken } = useAuthStore();
+	const { currentProfile } = useProfile();
+	const { isGuest, profileId } = useGuestStore();
+	const [refreshing, setRefreshing] = useState<boolean>(false);
 
-  const QueryRequest = {
-    sortCriteria: currentTag.name,
-    noRandomize: true,
-    publicationTypes: [PublicationTypes.Post],
-    metadata: {
-      mainContentFocus: [PublicationMainFocus.Video],
-    },
-    sources: ["lensplay", "lenstube"],
-    limit: 10,
-  };
+	const QueryRequest = {
+		sortCriteria: currentTag.name,
+		noRandomize: true,
+		publicationTypes: [PublicationTypes.Post],
+		metadata: {
+			mainContentFocus: [PublicationMainFocus.Video],
+		},
+		sources: ["lensplay", "lenstube"],
+		limit: 10,
+	};
 
-  const {
-    data: ExploreData,
-    error,
-    loading,
-    refetch,
-    fetchMore,
-  } = useExploreQuery({
-    variables: {
-      request: QueryRequest,
-      reactionRequest: {
-        profileId: isGuest ? profileId : currentProfile?.id,
-      },
-    },
-    context: {
-      headers: {
-        "x-access-token": `${!isGuest ? `Bearer ${accessToken}` : ""}`,
-      },
-    },
-  });
+	const {
+		data: ExploreData,
+		error,
+		loading,
+		refetch,
+		fetchMore,
+	} = useExploreQuery({
+		variables: {
+			request: QueryRequest,
+			reactionRequest: {
+				profileId: isGuest ? profileId : currentProfile?.id,
+			},
+		},
+		context: {
+			headers: {
+				"x-access-token": `${!isGuest ? `Bearer ${accessToken}` : ""}`,
+			},
+		},
+	});
 
-  useEffect(() => {
-    refetch({
-      request: QueryRequest,
-    });
-  }, [currentTag]);
+	useEffect(() => {
+		void refetch({
+			request: QueryRequest,
+		});
+	}, [currentTag]);
 
-  if (error)
-    return <ErrorMessage message={"Looks like something went wrong"} />;
+	if (error != null) return <ErrorMessage message={"Looks like something went wrong"} />;
 
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    try {
-      refetch({
-        request: QueryRequest,
-      })
-        .then(() => {
-          setRefreshing(false);
-        })
-        .catch((err) => {});
-    } catch (error) {
-    } finally {
-      setRefreshing(false);
-    }
-  }, []);
+	const onRefresh = React.useCallback(() => {
+		setRefreshing(true);
+		try {
+			refetch({
+				request: QueryRequest,
+			})
+				.then(() => {
+					setRefreshing(false);
+				})
+				.catch(() => {});
+		} catch (error) {
+		} finally {
+			setRefreshing(false);
+		}
+	}, []);
 
-  const Refresh = (
-    <RefreshControl
-      refreshing={refreshing}
-      onRefresh={onRefresh}
-      colors={[theme.PRIMARY]}
-      progressBackgroundColor={"black"}
-    />
-  );
+	const Refresh = (
+		<RefreshControl
+			refreshing={refreshing}
+			onRefresh={onRefresh}
+			colors={[theme.PRIMARY]}
+			progressBackgroundColor={"black"}
+		/>
+	);
 
-  const pageInfo = ExploreData?.explorePublications?.pageInfo;
+	const pageInfo = ExploreData?.explorePublications?.pageInfo;
 
-  const keyExtractor = (item: Explore) => item.id.toString();
+	const keyExtractor = (item: Explore): string => item.id.toString();
 
-  const ITEM_HEIGHT = 280;
+	const ITEM_HEIGHT = 280;
 
-  const getItemLayout = (_: any, index: number) => {
-    return {
-      length: ITEM_HEIGHT,
-      offset: ITEM_HEIGHT * index,
-      index,
-    };
-  };
-  TrackAction(EXPLORE.SWITCH_EXPLORE_FEED_TAB)
+	const getItemLayout = (_: any, index: number): any => {
+		return {
+			length: ITEM_HEIGHT,
+			offset: ITEM_HEIGHT * index,
+			index,
+		};
+	};
+	void TrackAction(EXPLORE.SWITCH_EXPLORE_FEED_TAB);
 
-  const onEndCallBack = () => {
-    if (!pageInfo?.next) {
-      // console.log("sab khatam ho gaya");
-      return;
-    }
-    fetchMore({
-      variables: {
-        request: {
-          cursor: pageInfo?.next,
-          ...QueryRequest,
-        },
-      },
-    }).catch((err) => {});
-  };
-  
-  const _MoreLoader = () => {
-    return (
-      <>
-        {pageInfo?.next ? (
-          <ActivityIndicator size={"large"} color={theme.PRIMARY} />
-        ) : (
-          <ErrorMessage message="No more Videos to load" withImage={false} />
-        )}
-      </>
-    );
-  };
+	const onEndCallBack = (): void => {
+		if (pageInfo?.next === null) {
+			// console.log("sab khatam ho gaya");
+			return;
+		}
+		fetchMore({
+			variables: {
+				request: {
+					cursor: pageInfo?.next,
+					...QueryRequest,
+				},
+			},
+		}).catch(() => {});
+	};
 
-  const MoreLoader = React.memo(_MoreLoader);
+	const _MoreLoader = (): JSX.Element => {
+		return (
+			<>
+				{pageInfo?.next === null ? (
+					<ActivityIndicator size={"large"} color={theme.PRIMARY} />
+				) : (
+					<ErrorMessage message="No more Videos to load" withImage={false} />
+				)}
+			</>
+		);
+	};
 
-  const RenderItem = ({ item }: { item: Explore }) => {
-    if (!item.hidden) {
-      return (
-        <VideoCard
-          key={`${item.id}-${item.createdAt}`}
-          publication={item as Explore}
-          id={item.id}
-        />
-        );
-      }
-      return <></>;
-    };
-    
-    return (
-      <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-      <ScrollView
-        style={{
-          height: 60,
-          paddingVertical: 8,
-          maxHeight: 60,
-          marginLeft: 10,
-        }}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-      >
-        {tags.map((item, index) => {
-          return (
-            <Pressable
-              android_ripple={{
-                color: "transparent",
-              }}
-              onTouchEndCapture={() => {
-                setCurrentTag(tags[index]);
-              }}
-              key={index}
-              style={{
-                marginHorizontal: 4,
-                backgroundColor: `${
-                  currentTag.name === item.name ? theme.PRIMARY : dark_primary
-                }`,
-                width: "auto",
-                maxHeight: 34,
-                paddingHorizontal: 12,
-                paddingVertical: 6,
-                justifyContent: "center",
-                alignItems: "center",
-                borderRadius: 8,
-              }}
-            >
-              <StyledText
-                title={item.name.replace(/_/g, " ")}
-                style={{
-                  fontSize: 12,
-                  fontWeight: "600",
-                  color: `${currentTag.name === item.name ? "black" : "white"}`,
-                }}
-              />
-            </Pressable>
-          );
-        })}
-      </ScrollView>
-      {loading ? (
-        <Skeleton children={<VideoCardSkeleton />} number={10} />
-      ) : (
-        <FlatList
-          data={ExploreData?.explorePublications.items as Explore[]}
-          keyExtractor={keyExtractor}
-          getItemLayout={getItemLayout}
-          initialNumToRender={3}
-          maxToRenderPerBatch={5}
-          ListFooterComponent={<MoreLoader />}
-          onEndReached={onEndCallBack}
-          onEndReachedThreshold={0.5}
-          refreshControl={Refresh}
-          renderItem={RenderItem}
-          showsVerticalScrollIndicator={false}
-        />
-      )}
-    </SafeAreaView>
-  );
+	const MoreLoader = React.memo(_MoreLoader);
+
+	const RenderItem = ({ item }: { item: Explore }): JSX.Element | null => {
+		if (!item.hidden) {
+			// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+			return <VideoCard key={`${item.id}-${item.createdAt}`} publication={item} id={item.id} />;
+		}
+		return null;
+	};
+
+	return (
+		<SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+			<ScrollView
+				style={{
+					height: 60,
+					paddingVertical: 8,
+					maxHeight: 60,
+					marginLeft: 10,
+				}}
+				horizontal={true}
+				showsHorizontalScrollIndicator={false}
+			>
+				{tags.map((item, index) => {
+					return (
+						<Pressable
+							android_ripple={{
+								color: "transparent",
+							}}
+							onTouchEndCapture={() => {
+								setCurrentTag(tags[index]);
+							}}
+							key={index}
+							style={{
+								marginHorizontal: 4,
+								backgroundColor: `${currentTag.name === item.name ? theme.PRIMARY : dark_primary}`,
+								width: "auto",
+								maxHeight: 34,
+								paddingHorizontal: 12,
+								paddingVertical: 6,
+								justifyContent: "center",
+								alignItems: "center",
+								borderRadius: 8,
+							}}
+						>
+							<StyledText
+								title={item.name.replace(/_/g, " ")}
+								style={{
+									fontSize: 12,
+									fontWeight: "600",
+									color: `${currentTag.name === item.name ? "black" : "white"}`,
+								}}
+							/>
+						</Pressable>
+					);
+				})}
+			</ScrollView>
+			{loading ? (
+				<Skeleton number={10}>
+					<VideoCardSkeleton />
+				</Skeleton>
+			) : (
+				<FlatList
+					data={ExploreData?.explorePublications.items as Explore[]}
+					keyExtractor={keyExtractor}
+					getItemLayout={getItemLayout}
+					initialNumToRender={3}
+					maxToRenderPerBatch={5}
+					ListFooterComponent={<MoreLoader />}
+					onEndReached={onEndCallBack}
+					onEndReachedThreshold={0.5}
+					refreshControl={Refresh}
+					renderItem={RenderItem}
+					showsVerticalScrollIndicator={false}
+				/>
+			)}
+		</SafeAreaView>
+	);
 }
