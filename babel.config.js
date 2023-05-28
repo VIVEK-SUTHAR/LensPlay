c554849781a04a89aae235bd5e6db2848b6b1ae8
Harsh <harshgajjar145@gmail.com>
2023-05-28 12:26:09 +0530

    fea: alias imports

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,26 @@
 module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin"],
-  };
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			[
+				'module-resolver',
+				{
+					alias: {
+						apollo: './apollo',
+						assets: './assets',
+						components: './components',
+						constants: './constants',
+						hooks: './hooks',
+						navigation: './navigation',
+						screens: './screens',
+						store: './store',
+						styles: './styles',
+						types: './types',
+						utils: './utils',
+					},
+				},
+			],
+		],
+	};
 };
