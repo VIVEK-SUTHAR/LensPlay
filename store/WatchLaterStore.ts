c49a1180c9562876375bb068d96b131fd21d0fd0
Harsh <harshgajjar145@gmail.com>
2023-06-05 16:32:34 +0530

    fea: watch later cover

--- store/WatchLaterStore.ts
+++ store/WatchLaterStore.ts
@@ -4,14 +4,29 @@ import create from "zustand";
 export type WatchLater = Post | Mirror;
 interface WatchLaterStore {
 	allWatchLaters: WatchLater[];
+	cover: null | string;
+	color: null | string;
 	setAllWatchLaters: (watchLaters: WatchLater[]) => void;
+	setCover: (cover: string) => void;
+	setColor: (color: string) => void;
 }
 
 const useWatchLater = create<WatchLaterStore>((set) => ({
 	allWatchLaters: [],
+	color: null,
+	cover: null,
 	setAllWatchLaters: (newWatchLaters) =>
 		set({
 			allWatchLaters: newWatchLaters,
 		}),
+	setCover: (cover) =>
+		set({
+			cover: cover,
+		}),
+	setColor: (color) =>
+		set({
+			color: color,
+		}),
 }));
+
 export default useWatchLater;
